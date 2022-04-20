efe2e32a0f5e416567e00e1f62930c64f882d77d
Krymyy <p-eetu@hotmail.com>
2022-04-20 13:57:30 +0300

    qr code lukija alku

--- app/src/main/java/square/ball/peppermobile/MainActivity.java
+++ app/src/main/java/square/ball/peppermobile/MainActivity.java
@@ -1,14 +1,58 @@
 package square.ball.peppermobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnScan;
+    String[] permissions = {
+            Manifest.permission.CAMERA
+    };
+    int PERM_CODE =  11;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkpermissions();
+
+        btnScan = (Button) findViewById(R.id.button);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ScannerActivity.class));
+            }
+        });
     }
+
+    private boolean checkpermissions(){
+        List<String> listofpermissions = new ArrayList<>();
+        for (String perm: permissions) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),perm) != PackageManager.PERMISSION_GRANTED){
+                listofpermissions.add(perm);
+            }
+        }
+
+        if (!listofpermissions.isEmpty()){
+            ActivityCompat.requestPermissions(this, listofpermissions.toArray(new String[listofpermissions.size()]), PERM_CODE);
+            return false;
+        }
+        return true;
+    }
+
+
 }
---
+++ app/src/main/java/square/ball/peppermobile/ScannerActivity.java
@@ -0,0 +1,53 @@
+package square.ball.peppermobile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+
+import javax.xml.transform.Result;
+
+public class ScannerActivity extends AppCompatActivity {
+
+    TextView txt;
+    CodeScanner codeScanner;
+    CodeScannerView codeScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+
+        txt = (TextView) findViewById(R.id.textView);
+        codeScannerView = (CodeScannerView) findViewById(R.id.scannerView);
+        codeScanner = new CodeScanner(this, codeScannerView);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull com.google.zxing.Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txt.setText(result.getText());
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestCamera();
+    }
+
+    private void requestCamera() {
+        codeScanner.startPreview();
+    }
+}
