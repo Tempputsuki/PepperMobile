c7afc7645c836d08b4e1950cacfdd89fdd3ff08b
Teemu <temppi166@gmail.com>
2022-04-20 13:06:44 +0300

    -Vaihdettu API level 26 -Lisätty Nappi

--- app/src/main/java/square/ball/peppermobile/MainActivity.java
+++ app/src/main/java/square/ball/peppermobile/MainActivity.java
@@ -2,13 +2,20 @@ package square.ball.peppermobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    Button one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        one.findViewById(R.id.kalle);
+
+
     }
 }
