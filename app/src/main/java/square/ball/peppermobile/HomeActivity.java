71387b68542ae02235c4d705c829414c22c3e4f1
Krymyy <p-eetu@hotmail.com>
2022-05-02 11:53:00 +0300

    qr code lukija alku ja avausikkunan aloitus

---
+++ app/src/main/java/square/ball/peppermobile/HomeActivity.java
@@ -0,0 +1,19 @@
+package square.ball.peppermobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    //protected <PlayBtn> void OnClickListener(PlayBtn) {
+
+    //}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
